74ed61583783bf4668344191c1d6275913b96b70
arondasamuel123 <arondasamuel123@gmail.com>
2020-04-01 15:32:54 +0300

    Created turf component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { TurfComponent } from './turf/turf.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    TurfComponent
   ],
   imports: [
     BrowserModule,
